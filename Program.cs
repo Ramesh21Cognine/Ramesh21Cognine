68300f481bee0f61e85f3e2a59d91c2c7ccdc90a
Ramesh21Cognine <ramesh.janapamula@cognine.com>
2021-05-07 14:00:08 +0530

    commit message

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            method1();
+        }
+
+        public static void method1()
+        {
+            m2();
+        }
+
+        private static void m2()
+        {
+            //throw new NotImplementedException();
+            m3();
+        }
+
+        private static void m3()
+        {
+            Console.WriteLine("This is method 3 called");
+        }
+    }
+}
